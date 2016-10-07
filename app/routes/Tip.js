9ea0d7a28ec0ee7ab316504e9c7740c34970fcf1
Mennanader <mennat.hafez@gmail.com>
2016-10-07 23:17:20 +0200

    debugging

--- app/routes/Tip.js
+++ app/routes/Tip.js
@@ -57,7 +57,7 @@ module.exports = function(router) {
             Tip.findById(req.query._id, function(err, tip) {
                 if (err)
                     res.send(err);
-                console.log(tip.disapprovals.find("ho"));
+                console.log([tip.disapprovals].find("ho"));
                 if (tip.disapprovals.includes(req.body.userid)) {
                     tip.disapprovals.pull(req.body.userid);
                 }
